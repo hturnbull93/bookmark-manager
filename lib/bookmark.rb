a638072744c00c10aa032ca35ebc8a1a355d3595
Harry Turnbull <hturnbull@live.co.uk>
2020-04-06 15:30:45 +0100

    Bookmark.all returns the three urls

---
+++ lib/bookmark.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Bookmark
+  def self.all
+    [
+      'http://www.makersacademy.com',
+      'http://www.destroyallsoftware.com',
+      'http://www.google.com'
+    ]
+  end
+end
