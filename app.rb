55205b745f7ac213e214a222a5d0e2e415655dbc
Harry Turnbull <hturnbull@live.co.uk>
2020-04-06 15:06:38 +0100

    rubocop lint

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
 
+# The class for Sinatra to use
 class BookmarkManager < Sinatra::Base
-
   get '/' do
-    "Bookmark Manager!"
+    'Bookmark Manager!'
   end
-
 end
