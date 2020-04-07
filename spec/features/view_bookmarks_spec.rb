bfad468f03975494bd8fbcc0e946ee7c7dc56723
Harry Turnbull <hturnbull@live.co.uk>
2020-04-07 22:45:04 +0100

    feature test has table data inserted

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -7,6 +7,14 @@ feature 'Viewing bookmarks' do
   end
 
   scenario 'bookmarks page has a list of bookmarks' do
+
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/')
     click_button 'View all'
     expect(page).to have_content 'http://www.makersacademy.com'
