b18cfc905942f5ecb3ce2997caed92884e830e80
Harry Turnbull <hturnbull@live.co.uk>
2020-04-07 22:27:16 +0100

    switched from hard array to db query

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      'http://www.makersacademy.com',
-      'http://www.destroyallsoftware.com',
-      'http://www.google.com'
-    ]
+    # Connect
+    connection = PG.connect(dbname: 'bookmark_manager')
+    # Execute query
+    result = connection.exec("SELECT * FROM bookmarks;")
+    # Map through array, table row is a hash, extract the value from the url key into an array
+    result.map { |bookmark| bookmark['url'] }
   end
 end
