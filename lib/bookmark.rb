04a01e7e2cad934424dc5e18999bd3d80560ffbb
Harry Turnbull <hturnbull@live.co.uk>
2020-04-07 22:36:33 +0100

    Bookmark.all logic to use test db based on ENV

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,11 +4,13 @@ require 'pg'
 
 class Bookmark
   def self.all
-    # Connect
-    connection = PG.connect(dbname: 'bookmark_manager')
-    # Execute query
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
     result = connection.exec("SELECT * FROM bookmarks;")
-    # Map through array, table row is a hash, extract the value from the url key into an array
     result.map { |bookmark| bookmark['url'] }
   end
 end
