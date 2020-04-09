f76567048f8a98b10dd2d1eacdf7cd99546eb3c2
Harry Turnbull <hturnbull@live.co.uk>
2020-04-09 17:51:31 +0100

    red: failing properly now

--- spec/features/deleting_bookmarks_spec.rb
+++ spec/features/deleting_bookmarks_spec.rb
@@ -1,11 +1,12 @@
 feature 'Deleting Bookmarks' do
   scenario ' can delete the first bookmark on the list' do
+    Bookmark.create(url: 'http://www.tobedeleted.com', title: 'Delete this')
     visit 'bookmarks'
-    expect(page).to have_link('Makers Academy', href: 'https://makersacademy.com')
+    expect(page).to have_link('Delete this', href: 'http://www.tobedeleted.com')
 
     first('.bookmark').click_button 'Delete'
 
     expect(current_path).to eq '/bookmarks'
-    expect(page).not_to have_link('Makers Academy', href: 'https://makersacademy.com')
+    expect(page).not_to have_link('Delete this', href: 'http://www.tobedeleted.com')
   end
 end
