8967a10c5d1da4bf5c843c6f54998778ce355614
Harry Turnbull <hturnbull@live.co.uk>
2020-04-09 11:38:21 +0100

    rf connection_type method out of .all and .create

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -4,23 +4,21 @@ require 'pg'
 
 class Bookmark
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
+    connection = self.connection_type
     result = connection.exec("SELECT * FROM bookmarks;")
     result.map { |bookmark| bookmark['url'] }
   end
 
   def self.create(url:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
+    connection = self.connection_type
     connection.exec("INSERT INTO bookmarks (url) VALUES ('#{url}')")
+  end
+
+  def self.connection_type
+    if ENV['ENVIRONMENT'] == 'test'
+      PG.connect(dbname: 'bookmark_manager_test')
+    else
+      PG.connect(dbname: 'bookmark_manager')
+    end
   end
 end
