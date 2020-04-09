da96ad7a2d1adddcccfe0b9cfb72a14c0c0dbb21
Harry Turnbull <hturnbull@live.co.uk>
2020-04-09 11:52:59 +0100

    red - add bookmarks with title

--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -1,18 +1,21 @@
 feature 'Adding bookmarks' do
   scenario 'user adds https://github.com' do
-    visit 'bookmarks/new'
+    fill_in('title', with: 'GitHub')
     fill_in('url', with: 'https://github.com')
+    visit 'bookmarks/new'
     click_button('Submit')
 
-    expect(page).to have_content 'https://github.com'
+    expect(page).to have_link('GitHub', href: 'https://github.com')
   end
 
   scenario 'user adds https://www.youtube.com' do
     visit 'bookmarks/new'
+
+    fill_in('title', with: 'YouTube')
     fill_in('url', with: 'https://www.youtube.com')
     click_button('Submit')
 
-    expect(page).to have_content 'https://www.youtube.com'
+    expect(page).to have_link('YouTube', href: 'https://www.youtube.com')
   end
 
   scenario 'user adds https://www.youtube.com, and sees all current bookmarks' do
@@ -22,12 +25,13 @@ feature 'Adding bookmarks' do
     Bookmark.create(url: 'http://www.google.com')
 
     visit 'bookmarks/new'
+    fill_in('title', with: 'YouTube')
     fill_in('url', with: 'https://www.youtube.com')
     click_button('Submit')
 
-    expect(page).to have_content 'https://www.youtube.com'
-    expect(page).to have_content 'http://www.makersacademy.com'
-    expect(page).to have_content 'http://www.destroyallsoftware.com'
-    expect(page).to have_content 'http://www.google.com'
+    expect(page).to have_link('YouTube', href: 'https://www.youtube.com')
+    expect(page).to have_link('YouTube', href: 'http://www.makersacademy.com')
+    expect(page).to have_link('YouTube', href: 'http://www.destroyallsoftware.com')
+    expect(page).to have_link('YouTube', href: 'http://www.google.com')
   end
 end
