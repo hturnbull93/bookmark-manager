775792b61469b04d81f18d86716a5d332f56b209
Harry Turnbull <hturnbull@live.co.uk>
2020-04-10 12:46:57 +0100

    R: feature test editing bookmark with invalid url

--- spec/features/validating_url_spec.rb
+++ spec/features/validating_url_spec.rb
@@ -1,11 +1,28 @@
 feature 'Validating URL input' do
-  scenario 'url: hello-world raises flash notice' do
+  scenario 'new bookmark url: hello-world raises flash notice' do
     visit '/bookmarks/new'
     fill_in('url', with: 'hello-world')
     fill_in('title', with: 'Any old title')
     click_button 'Submit'
 
     expect(page).to_not have_content 'hello-world'
+    expect(page).to have_content 'You must submit a valid URL'
+  end
+
+  scenario 'editing a bookmark url: hello-world raises flash notice' do
+    bookmark = Bookmark.create(url: 'https://www.mispelleddomain.com', title: 'Mispelled Title')
+
+    visit 'bookmarks'
+    expect(page).to have_link('Mispelled Title', href: 'https://www.mispelleddomain.com')
+    first('.bookmark').click_button 'Edit'
+
+    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
+    fill_in('title', with: 'YouTube')
+    fill_in('url', with: 'not valid url')
+    click_button 'Submit'
+
+    expect(current_path).to eq '/bookmarks'
+    expect(page).to_not have_link('YouTube', href: 'not valid url')
     expect(page).to have_content 'You must submit a valid URL'
   end
 end
