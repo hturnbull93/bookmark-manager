4b48f8be58362650e3abdf002a09a3a92680fdc8
Harry Turnbull <hturnbull@live.co.uk>
2020-04-10 12:47:40 +0100

    G: Bookmark.update validates url

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -11,7 +11,7 @@ class Bookmark
   end
 
   def self.create(url:, title:)
-    return false unless is_url?(url)
+    return false unless url?(url)
 
     result = DatabaseConnection.query("INSERT INTO bookmarks (title, url)
                                             VALUES ('#{title}', '#{url}')
@@ -19,12 +19,13 @@ class Bookmark
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
-
   def self.delete(id:)
     DatabaseConnection.query("DELETE FROM bookmarks WHERE id = #{id}")
   end
 
   def self.update(id:, title:, url:)
+    return false unless url?(url)
+
     result = DatabaseConnection.query("UPDATE bookmarks
                                           SET url = '#{url}', title = '#{title}'
                                         WHERE id = '#{id}'
@@ -47,8 +48,7 @@ class Bookmark
 
   private
 
-  def self.is_url?(url)
+  def self.url?(url)
     url =~ /\A#{URI::regexp(['http', 'https'])}\z/
   end
-
 end
