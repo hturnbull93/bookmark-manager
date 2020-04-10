eb35046f2fe95db00648fe73377828f23b61f376
Harry Turnbull <hturnbull@live.co.uk>
2020-04-10 14:26:27 +0100

    primed to switch to Comment.create

--- app.rb
+++ app.rb
@@ -56,9 +56,10 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/bookmarks/:id/comments' do
+    # Comment.create(text: params[:comment], bookmark_id: params[:id])
     connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO comments (text, bookmark_id)
-                          VALUES ('#{params[:comment]}', '#{params[:id]}');")
+    connection.exec("INSERT INTO comments (bookmark_id, text)
+                          VALUES ('#{params[:id]}', '#{params[:comment]}');")
     redirect '/bookmarks'
   end
 end
