5322d74d315f49c3ed68222ad54457fe8b2ef134
Harry Turnbull <hturnbull@live.co.uk>
2020-04-10 10:59:50 +0100

    R: db query sibmits exec to PG

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'database_connection'
 
 describe DatabaseConnection do
@@ -12,6 +14,14 @@ describe DatabaseConnection do
     it 'the connection is persistent' do
       connection = DatabaseConnection.setup('bookmark_manager_test')
       expect(DatabaseConnection.connection).to eq connection
+    end
+  end
+
+  describe '#query' do
+    it 'executes a query via PG' do
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+      expect(connection).to receive(:exec).with('SELECT * FROM bookmarks;')
+      DatabaseConnection.query('SELECT * FROM bookmarks;')
     end
   end
 end
