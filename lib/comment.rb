83a35ed284c376c904b5fbc0437d0355485a8ff4
Harry Turnbull <hturnbull@live.co.uk>
2020-04-10 22:24:02 +0100

    commented out debugging ps

--- lib/comment.rb
+++ lib/comment.rb
@@ -4,10 +4,28 @@ require_relative 'database_connection'
 
 class Comment
   def self.create(text:, bookmark_id:)
-    result = DatabaseConnection.query("INSERT INTO comments (bookmark_id, text)
-                                            VALUES ('#{bookmark_id}', '#{text}')
-                                         RETURNING id, text, bookmark_id;")
-    Comment.new(id: result[0]['id'], text: result[0]['text'], bookmark_id: result[0]['bookmark_id'])
+    # p "Comment.create called"
+    # p "the comment stuff"
+    # p text
+    # p "string?"
+    # p bookmark_id
+    # p "integer?"
+    # p bookmark_id.to_i
+    # p "the bookmark itself"
+    # bookmark = DatabaseConnection.query("SELECT * FROM bookmarks WHERE id = #{bookmark_id}")
+    # p bookmark[0]['id'].to_i.split("")
+
+    result = DatabaseConnection.query("INSERT INTO comments (bookmark_id, text) VALUES ('#{bookmark_id}','#{text}') RETURNING id, text, bookmark_id;")
+
+    # result = DatabaseConnection.query("INSERT INTO comments (text, bookmark_id)
+    #                                         VALUES ('#{text}', #{bookmark_id})
+    #                                      RETURNING id, text, bookmark_id;")
+    
+    Comment.new(
+      id: result[0]['id'],
+      text: result[0]['text'],
+      bookmark_id: result[0]['bookmark_id']
+    )
   end
 
   def self.where(bookmark_id:)
