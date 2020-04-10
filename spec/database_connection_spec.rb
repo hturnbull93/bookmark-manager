658a680767115aa12e76f7cb63ce042ac6dfbd26
Harry Turnbull <hturnbull@live.co.uk>
2020-04-10 10:38:44 +0100

    R: connection persists

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -1,11 +1,17 @@
 require 'database_connection'
-require 'pg'
 
 describe DatabaseConnection do
   describe '#setup' do
     it 'sets up a connection through PG' do
       expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
       DatabaseConnection.setup('bookmark_manager_test')
+    end
+  end
+
+  describe 'connection' do
+    it 'the connection is persistent' do
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+      expect(DatabaseConnection.connection).to eq connection
     end
   end
 end
