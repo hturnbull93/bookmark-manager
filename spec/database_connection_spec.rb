a8134dc19741c4ebd9c464402ef7a27cb38dc454
Harry Turnbull <hturnbull@live.co.uk>
2020-04-10 10:33:13 +0100

    R: DatabaseConnection.setup expects PG to recieve connect call

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -1,5 +1,9 @@
 require 'database_connection'
+require 'pg'
 
 describe DatabaseConnection do
-  
+  describe '#setup' do
+    expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
+    DatabaseConnection.setup('bookmark_manager_test')
+  end
 end
